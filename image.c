464257e031d794b19d713a779946a8847d685551
Madhu <enometh@net.meer>
2020-10-11 09:29:43 +0530

    Use black background in fullscreen mode
    SXIV:
    This reverts both
    
    28868767e Use normal win colors in fullscreen mode
    	Fixes issues #361 and #367
    
    07300da7d Do not keep track of fullscreen state
    
    NSXIV:
    - default bar bg  and bar fg to window fg and win bg
    - don't treat the bar specially in fullscreen mode.

--- image.c
+++ image.c
@@ -504,7 +504,7 @@ void img_render(img_t *img)
 			}
 			imlib_image_put_back_data(data);
 		} else {
-			XColor c = win->win_bg;
+			XColor c = win->fullscreen ? win->black : win->win_bg;
 			imlib_context_set_color(c.red >> 8, c.green >> 8, c.blue >> 8, 0xFF);
 			imlib_image_fill_rectangle(0, 0, dw, dh);
 		}
